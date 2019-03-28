1e14dfe943b2eef40a45f9fc989ba774eedea14f
walkerfloyd1 <walkerkeatley@yahoo.com>
2019-03-28 12:40:04 -0700

    newcommit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,12 @@
 $(document).on("ready", function() {
-    var queryUrl = "http://www.omdbapi.com/?s=" + title + "&i=tt3896198&apikey=45b10bed";
+    var queryUrl = "http://www.omdbapi.com/?s=" + title + "&plot=short&i=tt3896198&apikey=trilogy";
+    var title= "avengers";
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+    })
+    .then(function(response) {
+        console.log(response);
+    })
 })
