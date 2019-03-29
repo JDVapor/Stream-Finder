bef509d6ce619f09a4250104de00c234db9d0dcb
walkerfloyd1 <walkerkeatley@yahoo.com>
2019-03-29 14:10:17 -0700

    lodash-script-tag-and-api-keys

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 $(document).on("ready", function() {
-    var queryUrl = "http://www.omdbapi.com/?s=" + title + "&plot=short&i=tt3896198&apikey=trilogy";
-    var title= "avengers";
+    
+    //the name variable should be the value of the user's search.
+    var name = ??;
+    var queryUrl = "http://www.omdbapi.com/?apikey=trilogy&s=" + name + "&plot=short&";
+    
 
     $.ajax({
         url: queryUrl,
@@ -9,4 +12,17 @@ $(document).on("ready", function() {
     .then(function(response) {
         console.log(response);
     })
-})
+    var utellyQueryUrl = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack" ;
+    var term = name;
+
+    $.ajax({
+        url: utellyQueryUrl,
+        headers: {
+            "X-RapidAPI-Key": "f53d91c658msh3e2a2bf0273cc43p1b97a4jsn5ef72aa781b3"
+        },
+        method: "GET",
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+});
