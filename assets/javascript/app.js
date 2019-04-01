de9350cdd306186bd37a26ed7a56276055aa44eb
JDVapor <jdvapordestiny@gmail.com>
2019-04-01 14:28:15 -0700

    html update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,31 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAcsj4tXLbrb4kL-F7toML4Mt65-K9snnY",
+  authDomain: "streamfinder-32af6.firebaseapp.com",
+  databaseURL: "https://streamfinder-32af6.firebaseio.com",
+  projectId: "streamfinder-32af6",
+  storageBucket: "streamfinder-32af6.appspot.com",
+  messagingSenderId: "1019547739080"
+}
+firebase.initializeApp(config)
+var db = firebase.database();
+
 //firebase object for user accounts
 var fb = {
-  counter: 0,
-  // Initialize Firebase
-  config: {
-    apiKey: "AIzaSyAcsj4tXLbrb4kL-F7toML4Mt65-K9snnY",
-    authDomain: "streamfinder-32af6.firebaseapp.com",
-    databaseURL: "https://streamfinder-32af6.firebaseio.com",
-    projectId: "streamfinder-32af6",
-    storageBucket: "streamfinder-32af6.appspot.com",
-    messagingSenderId: "1019547739080"
-  },
-
-  init: function() {
-    firebase.initializeApp(config)
-  },
-
-  db: firebase.database(),
-
   createUser: function(email, password) {
+    console.log(email);
+    console.log(password);
+    debugger;
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      // ...
+      console.log("ERROR");
     })
   },
   getUser: function() {
     firebase.auth().onAuthStateChanged(function(user) {
+      console.log(user);
       if (user) {
         console.log(user);
         // User is signed in.
@@ -35,6 +33,16 @@ var fb = {
         console.log("no user signed in");
         // No user is signed in.
       }
+    });
+  },
+
+  signInUser: function(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      debugger;
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
     });
   }
 
@@ -49,23 +57,28 @@ var fb = {
   // }
 
 }
-
-fb.init();
-var newEmail = "test123@gmail.com";
-var newPW = "Password123"
-fb.createUser(newEmail, newPW);
-
 fb.getUser();
 
-// fb.writeUserData()
+$("#createAcc").on("click ", function() {
 
+  console.log('start');
+  var newEmail = $("#email").val();
+  console.log(newEmail);
+  var newPW = $("#password").val();
+  console.log(newPW);
+  fb.createUser(newEmail, newPW);
+  fb.signInUser(newEmail, newPW);
+
+
+  // fb.writeUserData()
+});
 
 
 //when submit button is clicked
 $("#searchBtn").on("click ", function() {
 
   //the input variable should be the value of the user's search.
-  var input = $("#userSearch").val();
+  var input = $("#userInput").val();
   var queryUrl = "http://www.omdbapi.com/?apikey=trilogy&s=" + input + "&plot=short&";
 
 
